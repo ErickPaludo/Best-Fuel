2b256c94ef3a38d4ea14948288cce9840017c4b5
Erick Paludo <erickpaludoyt@gmail.com>
2022-03-29 22:06:02 -0300

    Version 1

---
+++ The Best Fuel/Part_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Best_Fuel
+{
+    class Part_1
+    {
+
+        public int Média, Distancia;
+        public double Combustivel, Preço_0, Preço_1;
+
+        public Part_1(int média, double preço, int distancia)
+        {
+            Média = média;
+            Combustivel = preço;
+            Distancia = distancia;
+
+            Combustivel = distancia / média;
+            Preço_0 = Combustivel * preço;
+            Preço_1 = Preço_0 * 2;
+        }
+        public override string ToString()
+        {
+            return "Em média será gasto nessa viagem: R$ " + Preço_0.ToString("f2", CultureInfo.InvariantCulture) + " e para ida e volta será R$ " + Preço_1.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ The Best Fuel/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Best_Fuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part_1 extencion;
+
+            int x = 1;
+            while (x == 1)
+            {
+                Console.Write("Informe a média de Km/L do veículo: ");
+                int média = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.Write("Informe a o preço do combustível: ");
+                double preço = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.Write("Informe a Quilometragem que deseja percorrer: ");
+                int distancia = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                extencion = new Part_1(média, preço, distancia);
+                Console.WriteLine(extencion);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Executar outra viagem? (Sim = 1) (Não = 0)");
+                int y = int.Parse(Console.ReadLine());
+                if (y == 1)
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    x = 0;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(" -Beta 1.0- -Project_Best_Fuel");
+                }
+            }
+
+        }
+    }
+}
